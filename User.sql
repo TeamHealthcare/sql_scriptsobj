be15b073182bb7d4a5b4decf1551c2de9b2de3cd
coson <coson16@gmail.com>
2016-09-24 20:10:20 -0700

    Adding the initial script.

---
+++ User.sql
@@ -0,0 +1,12 @@
+CREATE TABLE User
+(
+    UserId      		int				NOT NULL	auto_increment	primary key,
+    EmployeeNumber      int    			NOT NULL,
+    JobTitle            varchar(255)    NOT NULL,
+    Password            varchar(32)     NOT NULL,
+    EmployeeName		varchar(64)		NOT NULL,
+    DateEntered			datetime		NOT NULL
+);
+
+INSERT INTO User (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (1111, 'Physician', 'doctor', 'Joe Smith', NOW());
+INSERT INTO User (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (1112, 'Nurse', 'nurse', 'Joe Intern', NOW());
