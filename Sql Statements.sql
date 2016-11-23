4ed4a45bbc63b58e3beedc1e5961be07e5346e06
coson <coson16@gmail.com>
2016-11-22 22:58:42 -0800

    Added necessary queries for User, Subsystem, and UserSubsystem objects

--- Sql Statements.sql
+++ Sql Statements.sql
@@ -8,3 +8,30 @@ VALUES ('Anthony Nelson', '714-293-1990', '234 S. Marin Dr', 'Huntington Beach',
 INSERT INTO electronicpatient (PatientName, PhoneNumber, Address, City, State, ZipCode, InsuranceCarrierId, DateOfBirth, Gender, Physician)
 VALUES ('Roger Healy', '305-749-1044', '914 Fetwick Dr', 'Cocoa Beach', 'FL', '33512', 4, '1970-11-15', 'M', 'Dr Huxtable');
 
+INSERT INTO subsystem (System) VALUES ('ElectronicPatient');
+INSERT INTO subsystem (System) VALUES ('MedicalEncounter');
+INSERT INTO subsystem (System) VALUES ('Dashboard');
+
+INSERT INTO user (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (111, 'Doctor', SHA1('Doctor'), 'Bob Jones', NOW());
+INSERT INTO user (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (112, 'Nurse', SHA1('Nurse'), 'Jackie Browne', NOW());
+INSERT INTO user (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (113, 'Therapist', SHA1('Therapist'), 'Glenda Thompson', NOW());
+INSERT INTO user (EmployeeNumber, JobTitle, Password, EmployeeName, DateEntered) VALUES (114, 'Unassigned', SHA1('Unassigned'), 'Jason Rodriguez', NOW());
+
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (17, 7);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (17, 8);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (17, 9);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (18, 7);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (18, 8);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (18, 9);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (19, 7);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (19, 9);
+INSERT INTO usersubsystem (UserId, SubSystemId) VALUES (20, 9);
+
+ALTER TABLE user
+ADD COLUMN `Username` VARCHAR(32) NOT NULL AFTER `JobTitle`;
+
+UPDATE user SET Username = 'bjones' WHERE EmployeeNumber = 111;
+UPDATE user SET Username = 'jbrowne' WHERE EmployeeNumber = 112;
+UPDATE user SET Username = 'gthompson' WHERE EmployeeNumber = 113;
+UPDATE user SET Username = 'jrodriguez' WHERE EmployeeNumber = 114;
+
